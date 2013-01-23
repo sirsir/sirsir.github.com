6e3f913436cb4e3263f54dbb236a0395993a638f
sirsir <sirsak@gmail.com>
2013-01-23 15:43:38 +0700

    First pages commit

---
+++ test.bat
@@ -0,0 +1,13 @@
+REM git clone https://github.com/sirsir/sirsir.github.com.git
+
+REM git checkout --orphan gh-pages
+
+
+REM git config --global user.email "sirsak@gmail.com"
+REM git config --global user.name "sirsir"
+
+REM cd "sirsir.github.com"
+REM echo "My GitHub Page" > index.html
+REM git add .
+git commit -a -m "First pages commit"
+git push -u origin gh-pages
